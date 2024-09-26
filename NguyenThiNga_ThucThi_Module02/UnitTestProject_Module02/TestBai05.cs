920a05ba0b6611603e951918504df7e1fe550056
Nguyen Nga <nguyennga200x@gmail.com>
2024-09-26 15:41:32 +0700

    module 2

---
+++ NguyenThiNga_ThucThi_Module02/UnitTestProject_Module02/TestBai05.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject_Module02
+{
+    [TestClass]
+    public class TestBai05
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+        
+        }
+    }
+}
+
