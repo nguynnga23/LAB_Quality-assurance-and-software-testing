902b1ed92765f84c75e9657c12963d53cce2c325
Nguyen Nga <nguyennga200x@gmail.com>
2024-09-26 15:49:33 +0700

    Module02_ThucThi

---
+++ NguyenThiNga_Module02_ThucThi/UnitTestProject_Module02/TestBai03.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject_Module02
+{
+    [TestClass]
+    public class TestBai03
+    {
+        //Test return True False
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool actualResult = o.primeCheck(17);
+            bool expectedResult = true;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool actualResult = o.primeCheck(25);
+            bool expectedResult = false;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool actualResult = o.primeCheck(0);
+            bool expectedResult = false;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool actualResult = o.primeCheck(1000);
+            bool expectedResult = false;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        //Test return Exception
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Exception expectedException = null;
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            try
+            {
+                o.primeCheck(-2);
+            }
+            catch (Exception ex)
+            {
+                expectedException = ex;
+            }
+            Assert.IsNotNull(expectedException);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Exception expectedException = null;
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            try
+            {
+                o.primeCheck(1020);
+            }
+            catch (Exception ex)
+            {
+                expectedException = ex;
+            }
+            Assert.IsNotNull(expectedException);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Exception expectedException = null;
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            try
+            {
+                o.primeCheck(-1);
+            }
+            catch (Exception ex)
+            {
+                expectedException = ex;
+            }
+            Assert.IsNotNull(expectedException);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Exception expectedException = null;
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            try
+            {
+                o.primeCheck(1001);
+            }
+            catch (Exception ex)
+            {
+                expectedException = ex;
+            }
+            Assert.IsNotNull(expectedException);
+        }
+    }
+}
---
+++ NguyenThiNga_Module02_ThucThi/UnitTestProject_Module02/TestBai04.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject_Module02
+{
+    [TestClass]
+    public class TestBai04
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
